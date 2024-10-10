e03c9b8a53d5deacd7a122623b97ddf46567b9f3
etsmith9@gmail.com <etsmith9@gmail.com>
2024-10-10 11:27:46 -0400

    updated code

--- question 3.py
+++ question 3.py
@@ -1,8 +1,6 @@
 year = int(input("Please enter a year to see if it is a leap year "))
         
-if year % 4 == 0 and year % 100 != 0:
-    print("This is a leap year!")
-elif year % 4 == 0 and year % 400 == 0:
+if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
     print("This is a leap year!")
 else:
     print("This is not a leap year!")
